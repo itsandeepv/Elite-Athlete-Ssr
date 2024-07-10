46f3fc740fb010ad868a8ab5d5fb56a06f7215f6
cetsandeepwork <159258159+cetsandeepwork@users.noreply.github.com>
2024-07-10 11:23:06 +0530

    all-produ df

--- next.config.js
+++ next.config.js
@@ -1,34 +1,25 @@
 const path = require('path');
 module.exports = {
-  images: {
-    domains: ['api.suppkart.com'],
-    deviceSizes: [320, 420, 768, 1024, 1200],
-  },
-  compress: true,
-  webpack: config => {
-    config.optimization.splitChunks = {
-      chunks: 'all',
-      minSize: 0,
-      maxInitialRequests: Infinity,
-      automaticNameDelimiter: '-',
-    };
-    return config
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/custom-route/:path*',
-        destination: '/another-path/:path*',
+    images: {
+        domains: ['https://api.suppkart.com'],
+        deviceSizes: [320, 420, 768, 1024, 1200],
+    },
+    compress: true,
+    webpack: config => {
+        config.optimization.splitChunks = {
+            chunks: 'all',
+            minSize: 0,
+            maxInitialRequests: Infinity,
+            automaticNameDelimiter: '-',
+        };
+        return config
+    },
+    async rewrites() {
+        return [
+          {
+            source: '/(.*)',
+            destination: '/',
+          },
+        ];
       },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/old-route',
-        destination: '/new-route',
-        permanent: true,
-      },
-    ];
-  },
 }
